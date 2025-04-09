4f6d5a9a3f1abd24fd8b21daac6f247e0d709c22
Sahak-prog-lgtm <sahakkarapetyan2007@gmail.com>
2025-04-09 19:00:31 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+
+
+// const barev = (text) => {
+//     return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+//   };
+
+//   console.log(barev("barev"));
+
+// const arr = [1,2,"", false, 0,true, [], {}, null, "sgsgs"]
+
+// const filterArr = arr.filter((item) => typeof item === "number");
+
+// console.log(filterArr);
+
+// const arr = [1,2,"", false, 0,true, [], {}, "value", null, "sgsgs"]
+
+// const filterArr = arr.filter((item) => typeof item === "object");
+
+// console.log(filterArr);
+
+
+const arr = [
+    {
+        title: "title1",
+            price: 50,
+        text: "text1",
+
+
+    },
+{
+    title: "title2",
+        price: 55,
+    text: "text2",
+
+
+},
+{
+    title: "title3",
+        price: 60,
+    text: "text3",
+
+
+},
+{
+    title: "title4",
+        price: 65,
+    text: "text4",
+
+
+},
+
+];
+
+// const value = prompt("hello")
+
+// const filterArr = arr.filter((item,) => item.title.includes(value) );
+
+// console.log(filterArr);
+
+    // const newArr = arr.map((item, index) => item.title + " " + index )
+
+    // console.log(newArr);
+
+    
