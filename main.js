b890fec9eb5c071589b2cd8eb52480bdc02febdf
Sahak Karapetyan <sahakkarapetyan2007@gmail.com>
2025-04-18 23:12:45 +0400

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const matches = [
 // Sample player data
 const players = [
     { name: 'Ռոման Թանթանյան', team: '10-11', goals: 12, assists: 6,  },
-    { name: 'Աբո Մանուկյան', team: '10-11', goals: 6, assists: 2, },
+    { name: 'Ալբերտ Մանուկյան', team: '10-11', goals: 6, assists: 2, },
     { name: 'Վահրամ Մարտիրոսյան', team: '9-3', goals: 4, assists: 0, }, 
     { name: 'Արթուր Բաղդագուլյան', team: '10-11', goals: 3, assists: 5,},
     { name: 'Ռուբեն Մաթևոսյան', team: '10', goals: 3, assists: 1, },
